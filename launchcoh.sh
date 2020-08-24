872a34b2d5aff83f5c8b4827c327d478eb011630
CrashCortez <46716213+CrashCortez@users.noreply.github.com>
2020-08-24 07:52:44 -0700

    Add files via upload

---
+++ launchcoh.sh
@@ -0,0 +1,2 @@
+cd "/home/pi/Games/coh"
+LD_LIBRARY_PATH=/home/pi/mesa/lib/arm-linux-gnueabihf ./penguinrocks.pl --launchonly --profile 1 --silentlaunch
---
+++ updatecoh.sh
@@ -0,0 +1,4 @@
+cd "/home/pi/Games/coh"
+./penguinrocks.pl --verify --patchonly 
+cd hc-bin32
+cp cityofheroes.exe /home/pi/Games/coh/hc-bin32cityofheroes.exe
