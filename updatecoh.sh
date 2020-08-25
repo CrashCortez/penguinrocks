7d01e119789373362bc49977c10b95971d639fb4
CrashCortez <46716213+CrashCortez@users.noreply.github.com>
2020-08-25 14:09:24 -0700

    Update updatecoh.sh

--- updatecoh.sh
+++ updatecoh.sh
@@ -2,3 +2,4 @@ cd "/home/pi/Games/coh"
 ./penguinrocks.pl --verify --patchonly 
 cd hc-bin32
 cp cityofheroes.exe /home/pi/Games/coh/hc-bin32cityofheroes.exe
+cp cityofheroes.exe /home/pi/Games/coh/hc-bin32/hc-bin32cityofheroes.exe
